c8b346770b6d6463fe6bc98f34b9e0ef65ed2ea0
Kevin Gisi <kevin@kevingisi.com>
2015-03-18 20:25:02 -0400

    More travis debugging

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -11,24 +11,24 @@ grunt build:dev
 echo "= Recreating test database"
 mongo "$TEST_DB" --eval "db.dropDatabase()"
 
-if [ -z "$TRAVIS" ]; then
-  ./node_modules/protractor/bin/webdriver-manager update
-  ./node_modules/protractor/bin/webdriver-manager start > /dev/null &
-  trap "curl http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer" EXIT
-fi
-
-# Wait for selenium
-MAX_WAIT=30
-WAITED=0
-until nc -z localhost 4444; do
-  if [ $WAITED -ge $MAX_WAIT ]; then
-    echo "Waited $MAX_WAIT seconds, but Selenium never responded" >&2
-    kill $NODE_PID
-    exit 1
-  fi
-  sleep 1
-  let 'WAITED+=1'
-done
+#if [ -z "$TRAVIS" ]; then
+#  ./node_modules/protractor/bin/webdriver-manager update
+#  ./node_modules/protractor/bin/webdriver-manager start > /dev/null &
+#  trap "curl http://localhost:4444/selenium-server/driver/?cmd=shutDownSeleniumServer" EXIT
+#fi
+#
+## Wait for selenium
+#MAX_WAIT=30
+#WAITED=0
+#until nc -z localhost 4444; do
+#  if [ $WAITED -ge $MAX_WAIT ]; then
+#    echo "Waited $MAX_WAIT seconds, but Selenium never responded" >&2
+#    kill $NODE_PID
+#    exit 1
+#  fi
+#  sleep 1
+#  let 'WAITED+=1'
+#done
 
 NODE_DB_URI="$TEST_DB_URI" PORT=$TEST_SERVER_PORT node ./website/src/server.js > /dev/null &
 NODE_PID=$!
